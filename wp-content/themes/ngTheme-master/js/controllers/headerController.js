ac263bd64c4038cecd8c0fbc35f48380cb369b0d
moller.peter <moller.g.peter@gmail.com>
2015-03-09 09:13:54 +0100

    Trying to understand the syntax by adding menu and content

--- wp-content/themes/ngTheme-master/js/controllers/headerController.js
+++ wp-content/themes/ngTheme-master/js/controllers/headerController.js
@@ -1,5 +1,15 @@
 //"ngTheme" controller.
-app.controller("headerController", ["$scope", "$location", function($scope, $location) {
+app.controller("headerController", ["$scope", "Pages", "$sce", "$location", function($scope, Pages, $sce, $location) {
   console.log("headerController is alive!");
+
+Pages.get();
+
+
+$scope.$on("gotPageData", function(event, data) {
+    // console.log("homeController on gotPageData: ", data);
+
+    $scope.trustedHtml = $sce.trustAsHtml(data[0].title);
+    // $scope.trustedHtml = $sce.trustAsHtml(data[0].title);
+  });
 
 }]);
--- wp-content/themes/ngTheme-master/js/controllers/homeController.js
+++ wp-content/themes/ngTheme-master/js/controllers/homeController.js
@@ -20,6 +20,7 @@ app.controller("homeController", ["$scope", "Pages", "$sce", function($scope, Pa
       and the ng-bind-html directive in the view
     */
     $scope.trustedHtml = $sce.trustAsHtml(data[0].content);
+    // $scope.trustedHtml = $sce.trustAsHtml(data[0].title);
   });
   
 }]);
