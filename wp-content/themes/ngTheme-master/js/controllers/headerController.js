0060c4c96f0b2777fc259b3e4921a22716ef823e
moller.peter <moller.g.peter@gmail.com>
2015-03-09 13:01:30 +0100

    no branch: trying to get out menu

--- wp-content/themes/ngTheme-master/js/controllers/headerController.js
+++ wp-content/themes/ngTheme-master/js/controllers/headerController.js
@@ -1,15 +1,18 @@
 //"ngTheme" controller.
 app.controller("headerController", ["$scope", "Pages", "$sce", "$location", function($scope, Pages, $sce, $location) {
-  console.log("headerController is alive!");
+  // console.log("headerController is alive!");
 
 Pages.get();
 
 
 $scope.$on("gotPageData", function(event, data) {
-    // console.log("homeController on gotPageData: ", data);
+    console.log("headerController I on gotPageData: ", data);
 
-    $scope.trustedHtml = $sce.trustAsHtml(data[0].title);
-    // $scope.trustedHtml = $sce.trustAsHtml(data[0].title);
+    for (var i = 0; i < data.length; i++) {
+    console.log("data title [i]", data[i].title);
+    $scope.trustedHtml = $sce.trustAsHtml(data[i].title);
+    }
+
   });
 
 }]);
--- wp-content/themes/ngTheme-master/js/controllers/homeController.js
+++ wp-content/themes/ngTheme-master/js/controllers/homeController.js
@@ -1,14 +1,14 @@
 //"ngTheme" home controller.
 //dependent on $scope && WPService being injected to run
 app.controller("homeController", ["$scope", "Pages", "$sce", function($scope, Pages, $sce) {
-  console.log("homeController alive!");
+  // console.log("homeController alive!");
   //get all pages
   Pages.get();
 
   // EXAMPLE LISTENER TO A $broadcast COMING FROM WPRest SERVICE!!!
   //listening for the "gotPageData" broadcast on $http success
   $scope.$on("gotPageData", function(event, data) {
-    console.log("homeController on gotPageData: ", data);
+    // console.log("homeController on gotPageData: ", data);
 
     /* 
       angular protects us from "dangerous" HTML by converting it to a string
