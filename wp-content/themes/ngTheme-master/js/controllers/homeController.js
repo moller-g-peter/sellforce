77b6fc172142f2a1c804048f27a7d25b4120724a
M-Ali-Th <m_ali1426@hotmail.com>
2015-03-17 11:00:48 +0100

    Home page slider

--- wp-content/themes/ngTheme-master/js/controllers/homeController.js
+++ wp-content/themes/ngTheme-master/js/controllers/homeController.js
@@ -1,7 +1,22 @@
 //"ngTheme" home controller.
 //dependent on $scope && WPService being injected to run
-app.controller("homeController", ["$scope", "Pages", "$sce", function($scope, Pages, $sce) {
+app.controller("homeController", ["$scope", "Pages", "$sce", "Property" ,"$routeParams", "SITE_INFO", function($scope, Pages, $sce, Property, $routeParams, SITE_INFO) {
   // console.log("homeController alive!");
+
+  Property.find($routeParams);
+  $scope.partialsDir = SITE_INFO.partials;
+
+  console.log("routeParams :",$routeParams);
+
+  //the interval for all carousels
+  $scope.carouselInterval = 2000;
+
+  $scope.$on("foundProperty", function(event, data) {
+    console.log("propertyController on foundProperty: ", data);
+    if (!data) { return; }
+    $scope.property = data[0];
+  });
+  
   //get all pages
   Pages.get(10);
 
@@ -23,7 +38,19 @@ app.controller("homeController", ["$scope", "Pages", "$sce", function($scope, Pa
     $scope.homePage = data;
 
 
+    // $scope.trustedHtml = $sce.trustAsHtml(data[2].content);
+
+
     // $scope.trustedHtml = $sce.trustAsHtml(data[0].title);
   });
+
+
+
+  //Property.find() accepts an object with key->value pairs that
+  //map to the search filters we need in our GET request
+
+
   
 }]);
+
+
--- wp-content/themes/ngTheme-master/js/services/propertyfactory.js
+++ wp-content/themes/ngTheme-master/js/services/propertyfactory.js
@@ -3,7 +3,7 @@ app.factory("Property", ["WPRest", "$sce", function (WPRest, $sce) {
   var propertyServant = {
     // endURL = last peace of the url.. 
     find : function(endURL) {
-      //
+      console.log("endURL: ",endURL);
       console.log("endURL in prop fack: ",endURL);
       //if no endURL, make empty obj so we dont crash!
       endURL = endURL ? endURL : {};
@@ -21,6 +21,7 @@ app.factory("Property", ["WPRest", "$sce", function (WPRest, $sce) {
         broadcastName: "bananName",// does this funky name run off for the async to prevent callvacfunction from run of before ig got its inparamter? 
         //send an object with the restCall = "callback" to deligate the acynk
         callback: function(postData) {
+          console.log("postDataaaaaaaa: ", postData);
 
           //if no results
           if (!postData) { return; // if no postData, end this function cause its no use running.. 
