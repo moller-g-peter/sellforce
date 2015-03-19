3f19bdda192a75ebac58f07843f4bd57ce097b53
M-Ali-Th <m_ali1426@hotmail.com>
2015-03-19 21:15:21 +0100

    footer styling!

--- wp-content/themes/ngTheme-master/js/controllers/contactsController.js
+++ wp-content/themes/ngTheme-master/js/controllers/contactsController.js
@@ -1,9 +1,10 @@
 app.controller("contactsController", ["$scope", "Pages", "$sce", function($scope, Pages, $sce) {
 
-console.log("contactsController workingnbnhvnbvnh!");
-Pages.get();
+// console.log("contactsController workingnbnhvnbvnh!");
+// Pages.get();
+
 $scope.$on("gotPageData", function(event, data) {
-    console.log("contactsController on: ", data);
+    console.log("- - - contactsController on: ", data);
 
     $scope.trustedHtml = $sce.trustAsHtml(data[1].content);
     // $scope.trustedHtml = $sce.trustAsHtml(data[0].title);
