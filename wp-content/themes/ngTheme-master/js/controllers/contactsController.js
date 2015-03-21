504a195ca32be7e5558375000ceb18c0ce31e3fa
M-Ali-Th <m_ali1426@hotmail.com>
2015-03-21 22:30:19 +0100

    Contacts page

--- wp-content/themes/ngTheme-master/js/controllers/contactsController.js
+++ wp-content/themes/ngTheme-master/js/controllers/contactsController.js
@@ -1,13 +1,26 @@
 app.controller("contactsController", ["$scope", "Pages", "$sce", function($scope, Pages, $sce) {
 
 // console.log("contactsController workingnbnhvnbvnh!");
-// Pages.get();
+Pages.get();
 
 $scope.$on("gotPageData", function(event, data) {
     console.log("- - - contactsController on: ", data);
 
-    $scope.trustedHtml = $sce.trustAsHtml(data[1].content);
+    $scope.trustedHtml = $sce.trustAsHtml(data[0].content);
     // $scope.trustedHtml = $sce.trustAsHtml(data[0].title);
   });
+	
+  // console.log("footerController is alive!");
+// Pages.search({name:"footer"});
+// var firstSearch = true;
+
+// $scope.$on("foundPages", function(event, data) {
+//     console.log("fffffffooterController on foundPages: ", data);
+//     if (firstSearch) {
+//     	// $scope.footerContent = $sce.trustAsHtml(data[0].content);
+//     	firstSearch = false;
+//     }
+//     $scope.trustedHtml = $sce.trustAsHtml(data[0].title);
+//   });
 
 }]);
