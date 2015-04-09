cbd8a696184ea0f652dec13c3b6e73f55abb1b07
dennislampert <dennis@lampert.se>
2015-04-09 15:03:24 +0200

    Having problem with some padding above menus in some kind of screensizes

--- wp-content/themes/ngTheme-master/index.php
+++ wp-content/themes/ngTheme-master/index.php
@@ -3,7 +3,7 @@
   <head>
     <!-- base href must correspond to the base path of your wordpress site -->
     <base href="/sellforce/">
-    <title>ngTheme</title>
+    <title>Sellforce</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <?php wp_head(); ?>
   </head>
